06226faa44a661bf19adeacf24d86dab3b0863bc
kinoshita <kinoshita@h40.n87.ibaraki.ac.jp>
2018-10-05 12:52:03 +0900

    Initial Commit

---
+++ Study20181005/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Study20181005
+//
+//  Created by kinoshita on 2018/10/05.
+//  Copyright © 2018年 kinoshitaShinya Kinoshita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
