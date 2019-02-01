efd075cfeedd6a659ad595d9e7f6a61d59332275
deco52 <deco52.s.k@gmail.com>
2019-02-01 14:14:05 +0900

    20190201 【内容】 ・POST通信で帰ってきたJSONパースのテスト 　→連想配列化して中身の取り出しに成功 　→ループでの階層ごとの取り出しも成功 【今後の予定】 ・レイアウトの確認(構築方法、リスト表示とそのイベント) ・Optional型の理解

--- Study20181005/ViewController2.swift
+++ Study20181005/ViewController2.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController2: UIViewController {
     
@@ -22,10 +23,10 @@ class ViewController2: UIViewController {
         //recieveLabel.lineBreakMode =
         // 受け取った前画面の値をセット
         recieveLabel.text = recieveText
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -43,25 +44,45 @@ class ViewController2: UIViewController {
         let task = URLSession.shared.dataTask(with: req, completionHandler: {
             (data, res, err) in
             if data != nil {
-                let text = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                //let text = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                let text = String(data: data!, encoding: String.Encoding.utf8)//, encoding: String.Encoding.utf8.rawValue)
                 DispatchQueue.main.sync(
                     execute: {
-                        self.recieveLabel.text = text as String?
-                        print(text)
-                    })
-                }
-            })
+                        print(text as Any)
+                        
+                        // jsonパーステスト
+                        let jsonData = text?.data(using: .utf8)
+                        do {
+                            let dic = try JSONSerialization.jsonObject(with: jsonData!,options: []) as? [String: Any]
+                            print(dic?["count"] as Any)
+                            
+                            //let roomInfo = dic?["0"] as? [String: Any]
+                            //print(roomInfo?["card_id"] as? String as Any)
+                            
+                            let count =  dic?["count"] as! Int
+                            for i in 0 ..< count {
+                                let roomInfo = dic?[String(i)] as? [String: Any]
+                                print(roomInfo?["identity_id"] as? String as Any) // Optional方なのでunwrapする必要があるらしい(nil判定？
+                                print(roomInfo?["card_id"] as? String as Any)
+                            }
+                        } catch {
+                            print(error)
+                        }
+                        
+                })
+            }
+        })
         task.resume()
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
